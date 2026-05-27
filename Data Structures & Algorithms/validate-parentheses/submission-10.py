2a14835a1de2ced91b5747a23cc037b9275c8e3b
iDako7 <117528529+iDako7@users.noreply.github.com>
2026-05-26 17:44:06 -0700

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # (]
+        # ([)]
+        # ()]
+
+        # stack to store openning brackets
+        opening = []
+        closing = { ")" : "(", "]" : "[", "}" : "{" }
+
+
+        # for each closing character, check if the top of stack is the same type
+        for c in s:
+            # if opening, add to stack
+            if c not in closing:
+                opening.append(c)
+            
+            # if closing, validate it
+            else:
+                # if opening and closing[c] != opening.pop():
+                #     return False
+                if not opening or closing[c] != opening.pop():
+                    return False
+
+        return not opening

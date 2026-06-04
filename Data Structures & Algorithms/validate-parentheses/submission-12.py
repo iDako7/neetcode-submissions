99b0633261b8b11fe46d13157e62979870787fff
iDako7 <117528529+iDako7@users.noreply.github.com>
2026-06-03 20:16:48 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # create a map to store the opening and closing pair
+        # for i in n
+            # popout == mapped closing
+        # return true
+
+        mapping = {"(": ")", "[" :"]", "{":"}"}
+
+        opening = []
+        for c in s:
+            if c in mapping:
+                opening.append(c)
+            else:
+                if mapping[opening.pop()] != c:
+                    return False
+        return True

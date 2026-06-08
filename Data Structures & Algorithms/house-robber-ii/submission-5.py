c64e170775bd88abfcdee419aa67f189491de0b3
iDako7 <117528529+iDako7@users.noreply.github.com>
2026-06-07 17:07:41 -0700

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        [2,9,8,3,6]
+        2,   10
+        2.      3
+          9     3
+          9        6
+        """
+
+
+        def dfs(i, flag):
+            if i >= len(nums):
+                return 0
+            
+            # start from head and bypass end
+            if i == len(nums) - 1 and flag:
+                return 0
+            
+            rob1 = nums[i] + dfs(i+2, flag)
+            rob2 = dfs(i+1, flag)
+            return max(rob1, rob2)
+        
+        rob0 = dfs(0, True)
+        rob1 = dfs(1, False)
+        return max(rob0, rob1)
